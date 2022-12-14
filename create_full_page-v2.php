36d47cdcf660930ec53198dc00c99c033667bcbc
Jean-Marc <garciajeanmarc@gmail.com>
2022-12-14 22:43:31 +0100

    reorganizing html files in progress...

---
+++ create_full_page-v2.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once("hfjdklshqfkdlsq.php");
+
+$full_page = '
+    <!DOCTYPE html>
+        <html lang="fr">
+        <head>' . $head_content . '</head>
+        <body>
+            <header>' . $header_content . '</header>
+            <main>' . $main_content . '</main>
+            <footer>' . $footer_content . '</footer>
+            <noscript>' . $noscript_content . '</noscript>
+        </body>
+    </html>';
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+$test_html_in_var = file_get_contents("test_2.html");
+
+echo $test_html_in_var;
