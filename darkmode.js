6db4cb6c6651701fd15b37c4eca38558f41ec5c5
Jean-Marc <garciajeanmarc@gmail.com>
2023-01-10 16:29:00 +0100

    refactoring css + add dark mode test

---
+++ darkmode.js
@@ -0,0 +1,6 @@
+const switchButton = document.getElementById("switch_button");
+const switchZone = document.getElementById("switch_zone");
+
+function switchMode() {
+    switchZone.classList.toggle("dark_mode");
+}
