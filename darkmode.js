7d634ec33ff7d008cab1cac0b8871cbf2d8bcace
Jean-Marc <garciajeanmarc@gmail.com>
2023-01-13 16:06:53 +0100

    header / css / dark mode

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,3 @@
-const switchButton = document.getElementById("switch_button");
-const switchZone = document.getElementById("switch_zone");
-
 function switchMode() {
-    switchZone.classList.toggle("dark_mode");
+    document.body.classList.toggle("dark_mode");
 }
---
+++ nav.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById("header_nav");
+const logo = document.getElementById("logo");
+
+function showNavMenu() {
+    nav.classList.toggle("nav_show");
+}
