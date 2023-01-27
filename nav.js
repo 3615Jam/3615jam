0b69f3309105f11d071a3a4d89914d0a16a15bb6
3615jam <garciajeanmarc@gmail.com>
2023-01-28 00:09:59 +0100

    working on nav menu

--- nav.js
+++ nav.js
@@ -1,5 +1,4 @@
-const nav = document.getElementById("header_nav");
-const logo = document.getElementById("logo");
+const showNavButton = document.getElementById("show_nav_menu");
 
 function showNavMenu() {
     nav.classList.toggle("nav_show");
