27b36465b2a68ea875f660163b71ccb0a244a991
Jean-Marc <garciajeanmarc@gmail.com>
2023-02-13 12:33:38 +0100

    css slider menu

---
+++ nav_test_4.js
@@ -0,0 +1,20 @@
+const nav = document.getElementById("navnav");
+const centralDisplay = document.getElementById("central_display");
+
+function toggleNavMenu() {
+    window.innerWidth < 1080 ? nav.classList.toggle("slide_up") : centralDisplay.classList.toggle("slide_right");
+    console.log("##### toggleNavMenu OK #####");
+}
+
+// ------------------------------------------------------------
+// mise à jour de l'afficheur de résolution de la fenêtre :
+
+const widthDisplay = document.getElementById("width");
+
+function updateWidthDisplay() {
+    widthDisplay.textContent = window.innerWidth;
+}
+
+updateWidthDisplay();
+
+window.addEventListener("resize", updateWidthDisplay);
