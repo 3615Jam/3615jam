70bf9c7c33a8290fb6422ad7e134a0ed3c420e0c
Jean-Marc <garciajeanmarc@gmail.com>
2023-04-18 13:52:00 +0200

    updated nav menu for mobile devices + dark icons

--- darkmode.js
+++ darkmode.js
@@ -1,20 +1,36 @@
-const darkModeSwitch = document.getElementsByClassName("dark_mode_switch");
+const darkModeSwitch = document.getElementById("dark_mode_switch");
 const formZoneText = document.getElementsByClassName("form_zone_text");
 const legendLogo = document.getElementsByClassName("legend_logo");
+const icoMail = document.getElementById("ico_mail");
+const icoPhone = document.getElementById("ico_phone");
 
 function switchMode() {
     // switch le body en mode dark
     document.body.classList.toggle("dark_mode");
-    // cache l'élément 'lune' en mode dark ou l'élément 'soleil' en mode light
-    Array.from(darkModeSwitch).forEach((element) => {
-        element.classList.toggle("hidden");
-    });
+
+    // [V1] cache l'élément 'lune' en mode dark ou l'élément 'soleil' en mode light
+    // Array.from(darkModeSwitch).forEach((element) => {
+    //     element.classList.toggle("hidden");
+    // });
+    // [V2] change la source de l'image 'lune' pour celle de l'image 'soleil'
+    darkModeSwitch.classList.toggle("sun");
+
     // swtich les zones de texte et input en mode dark
     Array.from(formZoneText).forEach((element) => {
         element.classList.toggle("dark_mode_input");
     });
-    // switch les logos noir en blanc en mode dark
-    Array.from(legendLogo).forEach((element) => {
-        element.classList.toggle("dark_mode_img");
-    });
+
+    // switch les logos noir en blanc en mode dark :
+
+    // [V1] : en applicant un filtre 'invert' à chaque élément
+    // Array.from(legendLogo).forEach((element) => {
+    //     element.classList.toggle("dark_mode_img");
+    // });
+
+    // [V2] : en changeant la source de chaque image
+    if (icoMail) {
+        icoMail.classList.toggle("ico_mail_white");
+        icoPhone.classList.toggle("ico_phone_white");
+        console.log("coucou icoMail !");
+    }
 }
